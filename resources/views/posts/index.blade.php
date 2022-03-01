a2430b9068330b31f10ebcba3e50299244fe7b11
Wilson Castillo <wilxoncastillo@gmail.com>
2022-03-01 12:00:00 -0500

    Mostrar el detalle de un post II

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,13 +8,13 @@
                 <div class="w-full h-full px-8 flex flex-col justify-center">
                     <div >
                         @foreach($post->tags as $tag)
-                            <a class="inline-block px-3 h-6 text-white rounded-full bg-{{ $tag->color }}-500" href="">{{ $tag->name }}</a>
+                            <a href="" class="inline-block px-3 h-6 text-white rounded-full bg-{{ $tag->color }}-500" >{{ $tag->name }}</a>
                             
                         @endforeach
                     </div>
                     
                     <h1 class="text-4xl text-white leading-8 font-bold">
-                        <a href="">{{ $post->name }}</a>
+                        <a href="{{ route('posts.show', $post) }}">{{ $post->name }}</a>
                     </h1>
 
                 </div>
