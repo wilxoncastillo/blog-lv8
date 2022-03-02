aa9616c49a1c2ed4520ab5c6f6f8137a019781ce
Wilson Castillo <wilxoncastillo@gmail.com>
2022-03-02 08:59:39 -0500

    Filtrar los posts por etiquetas

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -10,7 +10,7 @@ class Tag extends Model
     use HasFactory;
 
     // relacion n:n
-    public function post() {
+    public function posts() {
         return $this->belongsToMany(Post::class);
     }
 }
---
+++ resources/views/components/card-post.blade.php
@@ -0,0 +1,26 @@
+@props(
+    [
+        'post'
+    ])
+
+<article class="mb-8 bg-white shadow-lg rounded-lg overflow-hidden">
+    <img src="{{ Storage::url($post->image->url) }}" class="w-full h-72 object-cover object-center" alt="">
+
+    <div class="px-6 py-4">
+        <h1 class="font-bold text-xl mb-2">
+            <a href="{{ route('posts.show', $post) }}">{{ $post->name }}</a>
+        </h1>
+
+        <div class="text-gray-700 text-base">
+            {{ $post->extract}}
+        </div>
+
+        <div class="px-6 pt-4 pb-2">
+            @foreach($post->tags as $tag)
+                <a href="{{ route('posts.tag', $tag) }}" class="inline-block px-3 text-white text-sm rounded-full bg-{{ $tag->color }}-500 mr-2" >{{ $tag->name }}</a>
+            @endforeach
+
+        </div>
+
+    </div>
+</article>
